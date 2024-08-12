2d1032c4d17295d38e8d99862d5f17abf4a0f800
Elijah Ang <eli.axel.ang@gmail.com>
2024-08-12 16:41:06 +0800

    It builds!

---
+++ src/client/java/io/github/simplexdev/quickcoordscopy/QuickCoordsCopyClient.java
@@ -0,0 +1,48 @@
+package io.github.simplexdev.quickcoordscopy;
+
+import org.lwjgl.glfw.GLFW;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
+
+public class QuickCoordsCopyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        final var DEFAULT_COORDS_FORMAT = "$x, $y, $z";
+        final var COORDS_FORMAT_KEY = "formatting";
+        final var DEFAULT_CONFIG = COORDS_FORMAT_KEY + "=" + DEFAULT_COORDS_FORMAT;
+
+        final var config = SimpleConfig
+            .of("quickcoordscopy")
+            .provider((namespace) -> DEFAULT_CONFIG)
+            .request();
+
+        final var copyCoordsKey = KeyBindingHelper
+            .registerKeyBinding(
+                    new KeyBinding(
+                            "key.quickcoordscopy.copy",
+                            InputUtil.Type.KEYSYM,
+                            GLFW.GLFW_KEY_GRAVE_ACCENT,
+                            "category.quickcoordscopy.main"));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (copyCoordsKey.wasPressed()) {
+                if (client.player != null) {
+                    final var format = config.getOrDefault(COORDS_FORMAT_KEY, DEFAULT_COORDS_FORMAT);
+
+                    final var coords = format
+                            .replace("$x", String.valueOf(Math.round(client.player.getX())))
+                            .replace("$y", String.valueOf(Math.round(client.player.getY())))
+                            .replace("$z", String.valueOf(Math.round(client.player.getZ())));
+
+                    client.keyboard.setClipboard(coords);
+                    client.player.sendMessage(Text.literal("Copied coordinates to keyboard!"));
+                }
+            }
+        });
+    }
+}
